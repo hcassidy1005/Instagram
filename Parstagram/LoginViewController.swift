e9aade64806869de2cbb51228e8d0c3b0218c0f6
Hailey Cassidy <HaileyCassidy@macbook-pro-5.attlocal.net>
2019-03-24 21:30:41 -0500

    User stays logged in across restarts

--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -16,8 +16,21 @@ class LoginViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        //Looks for single or multiple taps.
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        
+        //Uncomment the line below if you want the tap not not interfere and cancel other interactions.
+        //tap.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tap)
         // Do any additional setup after loading the view.
+    }
+    
+    //Calls this function when the tap is recognized.
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        print("HERE!!")
+        view.endEditing(true)
     }
     
     @IBAction func onSignIn(_ sender: Any) {
@@ -48,6 +61,9 @@ class LoginViewController: UIViewController {
         
         
     }
+    
+
+    
     
     /*
     // MARK: - Navigation
